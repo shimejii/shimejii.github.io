d7719d3aa16c39bf6d85ee1ece22f83cdc684b4b
oitama <oystermushroom.om@gmail.com>
2026-06-14 14:41:21 +0900

    add: 資格ソート機能, update: cssの修正

--- main.js
+++ main.js
@@ -3,29 +3,48 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await res.json();
 
     const container = document.querySelector(".certification");
-
-    data.certifications.forEach((cert) => {
-
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        const title = document.createElement("h3");
-        title.textContent = cert.name;
-
-        const label = document.createElement("div");
-        label.classList.add("date-label");
-        label.textContent = `取得年月: ${cert.year}/${cert.month}`;
-
-        card.appendChild(label);
-        card.appendChild(title);
-        container.appendChild(card);
-    });
-
     const radios = document.querySelectorAll('input[name="sort"]');
+
+    let certifications= data.certifications;
+
+    function render(certs) {
+        container.innerHTML = "";
+
+        certs.forEach(cert => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+
+            const title = document.createElement("h3");
+            title.textContent = cert.name;
+
+            const label = document.createElement("div");
+            label.classList.add("date-label");
+            label.textContent = `取得年月: ${cert.year}/${cert.month}`;
+
+            card.appendChild(label);
+            card.appendChild(title);
+            container.appendChild(card);
+        });
+    }
+
+    render(certifications);
+
     radios.forEach(radio => {
-        radio.addEventListener('change', () => {
-            container.classList.add('stack');
+        radio.addEventListener("change", () => {
+            let sorted = [...certifications];
+
+            if (document.getElementById("opt1").checked) {
+                sorted.sort((a,b) =>
+                    a.year - b.year || a.month - b.month
+                );
+            }
+
+            if (document.getElementById("opt2").checked) {
+                sorted.sort((a,b) =>
+                    a.name.localeCompare(b.name, "ja")
+                );
+            }
+            render(sorted)
         });
     });
-
 });
